f4c735be41aa8448a38d1c7728e42b62a142fe2a
Ddobysh27 <dobysh.dmitry27@gmail.com>
2020-02-06 17:39:27 +0300

    jsp

--- src/main/java/net/proselyte/jwtappdemo/rest/TestHeroku.java
+++ src/main/java/net/proselyte/jwtappdemo/rest/TestHeroku.java
@@ -1,4 +1,27 @@
 package net.proselyte.jwtappdemo.rest;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Controller
 public class TestHeroku {
+    @Bean
+    ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
+
 }
